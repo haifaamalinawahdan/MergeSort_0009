40c918d0c5f9643ab808077069154e65b69de7af
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-02 15:16:02 +0700

    membuat if else condition

--- mergesort.cpp
+++ mergesort.cpp
@@ -12,6 +12,15 @@ void input ()
    {
       cout << "Masukkan panjang element array : ";
       cin >> n;
+
+      if (n <= 20)
+      {
+        break;
+      }
+      else
+      {
+        cout << "\nMaksimal panjang array adalah 20";
+      }
    }
 
 }
